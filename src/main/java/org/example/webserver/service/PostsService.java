c8536f4625ff5be31ae7f065442baa03044cb599
Leekyungun <leekyungun91@gmail.com>
2021-09-23 22:22:00 +0900

    Posts 수정 조회 기능 구현

--- src/main/java/org/example/webserver/service/PostsService.java
+++ src/main/java/org/example/webserver/service/PostsService.java
@@ -1,8 +1,11 @@
 package org.example.webserver.service;
 
 import lombok.RequiredArgsConstructor;
+import org.example.webserver.domain.posts.Posts;
 import org.example.webserver.domain.posts.PostsRepository;
+import org.example.webserver.web.dto.PostsResponseDto;
 import org.example.webserver.web.dto.PostsSaveRequestDto;
+import org.example.webserver.web.dto.PostsUpdateRequestDto;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
@@ -13,7 +16,21 @@ public class PostsService {
     private final PostsRepository postsRepository;
 
     @Transactional
-    public Long save(PostsSaveRequestDto requestsDto){
+    public Long save(PostsSaveRequestDto requestsDto) {
         return postsRepository.save(requestsDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+        return id;
+    }
+
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+        return new PostsResponseDto(entity);
     }
 }
