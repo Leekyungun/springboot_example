994ded9d0260952f9bee4384d333392a6a67c80d
Leekyungun <leekyungun91@gmail.com>
2021-10-02 20:15:08 +0900

    게시글 전체 조회 페이지 만들기

--- src/main/java/org/example/webserver/service/PostsService.java
+++ src/main/java/org/example/webserver/service/PostsService.java
@@ -3,12 +3,15 @@ package org.example.webserver.service;
 import lombok.RequiredArgsConstructor;
 import org.example.webserver.domain.posts.Posts;
 import org.example.webserver.domain.posts.PostsRepository;
+import org.example.webserver.web.dto.PostsListResponseDto;
 import org.example.webserver.web.dto.PostsResponseDto;
 import org.example.webserver.web.dto.PostsSaveRequestDto;
 import org.example.webserver.web.dto.PostsUpdateRequestDto;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -32,5 +35,13 @@ public class PostsService {
         Posts entity = postsRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+
     }
 }
